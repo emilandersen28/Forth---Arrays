e4901b8f9226ea456f135b269ff46c966fef63b8
AskVorting <ask@boegedal.com>
2021-10-08 12:26:54 +0200

    commit 1

--- push_pop.c
+++ push_pop.c
@@ -18,28 +18,3 @@ int first;
         }
 }
 
-
-int main(void){
-int size=10;
-int main_stack[10]={0,0,0,0,0,0,0,0,0,0};
-int stack_size;
-//scanin numbers
-
-printf("Enter values, end with 0\n");
-
-for(int i=0;i<size;i++){
-    scanf("%d\n",&main_stack[i]);
-    stack_size=i+1;
-    if(main_stack[i]==0);
-        i=size-1;
-}
-
-
-
-//calls push functions
-push_first(main_stack,x);
-push_first(main_stack,y);
-for(int i=0;i<size;i++){
-printf(" %d , %d",main_stack[0],main_stack[1]);
-return 0;
-}
